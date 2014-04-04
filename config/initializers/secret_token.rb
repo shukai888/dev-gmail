bf81888ab869ef6b50560a881199b711cc3d2c99
shukai888 <shukai888@gmail.com>
2014-04-04 20:16:05 +0800

    device with gmail comfirm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DevGmail::Application.config.secret_token = '672e8cfdf9c8734229e0d588c12a126f9da585e1602d3b4bc47d8b43a1e2baa28995ee54c5daf88917f58e5c47edfb9b047139b6a33cacef042e1323cb33bb27'
